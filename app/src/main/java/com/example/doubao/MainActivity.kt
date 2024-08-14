21698883ce3d15e4a49f5b93e528240fe183bffe
yaoweima <ma_yaowei@outlook.com>
2024-08-14 14:09:08 -0700

    Implemented a simple example of Post Notification and Intent

--- app/src/main/java/com/example/doubao/MainActivity.kt
+++ app/src/main/java/com/example/doubao/MainActivity.kt
@@ -1,7 +1,15 @@
 package com.example.doubao
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.NotificationManager.IMPORTANCE_DEFAULT
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavGraphNavigator
+import com.example.doubao.model.MyDataForTest
+import com.example.doubao.ui.profile.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +17,31 @@ class MainActivity : AppCompatActivity() {
     setContentView(R.layout.activity_main)
     // Adding a fragment programmatically. (if use navigation, then don't need and will cause bug)
     // supportFragmentManager.beginTransaction().add(R.id.root, ProfileFragment(), "CHILD_FRAGMENT").commitNow()
+
+    // Create the notification channel
+    createNotificationChannel(this)
+
+    // check if the activity was started with an Intent that has the action "OPEN_PROFILE_FRAGMENT"
+    if (intent?.action == "OPEN_PROFILE_FRAGMENT") {
+      val myData = intent.getParcelableExtra("CUSTOM_DATA", MyDataForTest::class.java)
+      if (myData != null) {
+        Log.i("tag", "received not null myData! Success")
+      } else {
+        Log.i("tag", "received NULL myData!")
+      }
+    }
+  }
+
+  /** Create the doubao app channel for notification. */
+  private fun createNotificationChannel(context: Context) {
+    val channelId = "doubao_channel_id"
+    val channelName = "Doubao"
+    val channelDescription = "Doubao cat channel"
+    val channel = NotificationChannel(channelId, channelName, IMPORTANCE_DEFAULT).apply {
+      description = channelDescription
+    }
+
+    val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    notificationManager.createNotificationChannel(channel)
   }
 }
--- app/src/main/java/com/example/doubao/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/doubao/ui/profile/ProfileFragment.kt
@@ -1,13 +1,23 @@
 package com.example.doubao.ui.profile
 
+import android.Manifest
+import android.app.PendingIntent
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationCompat.PRIORITY_DEFAULT
+import androidx.core.app.NotificationManagerCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.doubao.MainActivity
 import com.example.doubao.R
 import com.example.doubao.databinding.FragmentProfileBinding
+import com.example.doubao.model.MyDataForTest
 
 /**
  * The entrance of doubao App. Showing doubao photos and buttons to gallery and list.
@@ -31,6 +41,51 @@ class ProfileFragment : Fragment() {
     }
     binding.listButton.setOnClickListener {
       findNavController().navigate(R.id.action_profileFragment_to_listFragment)
+    }
+
+    // For notification test:
+    binding.notificationButton.setOnClickListener {
+      setUpAndPostNotification()
+    }
+  }
+
+  private fun setUpAndPostNotification() {
+    val myDataForTest = MyDataForTest("Value in Intent")
+
+    val intent = Intent(activity, MainActivity::class.java).apply {
+      putExtra("CUSTOM_DATA", myDataForTest)
+      action = "OPEN_PROFILE_FRAGMENT"
+    }
+
+    // Wrap the Intent with a PendingIntent (A description of an Intent and target action to perform with it.)
+    val pendingIntent: PendingIntent = PendingIntent.getActivity(
+      activity,
+      0,
+      intent,
+      PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+    )
+
+    // TODO: see how to set automatically dismiss
+    val notification = NotificationCompat.Builder(requireContext(), "doubao_channel_id")
+      .setSmallIcon(R.drawable.ic_launcher_foreground)
+      .setContentTitle("Test Notification from Doubao")
+      .setContentText("Click to open Profile with a customized string")
+      .setPriority(PRIORITY_DEFAULT)
+      .setContentIntent(pendingIntent)
+      .setAutoCancel(true)
+      .setTimeoutAfter(60000) // cancel after certain time (1min here).
+      .build()
+
+    with(NotificationManagerCompat.from(requireContext())) {
+      if (ActivityCompat.checkSelfPermission(
+          requireContext(),
+          Manifest.permission.POST_NOTIFICATIONS
+        ) != PackageManager.PERMISSION_GRANTED
+      ) {
+        // do nothing here because the post notificaiton should be granted.
+        return@with
+      }
+      notify(1, notification)
     }
   }
 }
