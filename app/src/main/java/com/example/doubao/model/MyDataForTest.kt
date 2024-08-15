c379a41f4ac40589fc73fb372218f38936cbffea
yaoweima <ma_yaowei@outlook.com>
2024-08-15 14:07:42 -0700

    Add comments for what can be the Parcelable elements

--- app/src/main/java/com/example/doubao/model/MyDataForTest.kt
+++ app/src/main/java/com/example/doubao/model/MyDataForTest.kt
@@ -1,7 +1,15 @@
 package com.example.doubao.model
 
 import android.os.Parcelable
+import java.lang.StringBuilder
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class MyDataForTest(val content: String) : Parcelable
+data class MyDataForTest(val content: String, val customizedClass: CustomizedClass) : Parcelable
+
+@Parcelize
+// Each field needs to be parcelable/serializable
+class CustomizedClass : Parcelable {
+  val customizedField: String = "10"
+  val somethingElse: StringBuilder = StringBuilder() // StringBuilder implemented Serializable.
+}
